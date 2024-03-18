422d07d687bf2c1f1597a1b177754b625aa8e8db
tonysodeep <hynguyen2727@gmail.com>
2024-03-18 17:43:29 +0700

    request access mic permission

--- app/src/main/java/com/example/spyrec/MainActivity.kt
+++ app/src/main/java/com/example/spyrec/MainActivity.kt
@@ -1,20 +1,53 @@
 package com.example.spyrec
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
+import com.example.spyrec.databinding.ActivityMainBinding
+
+
+const val REQUEST_CODE = 200
 
 class MainActivity : AppCompatActivity() {
+    private var permissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private var permissionGranted = false
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        permissionGranted = ActivityCompat.checkSelfPermission(
+            this,
+            permissions[0]
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (!permissionGranted)
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+
+        binding.btnRecord.setOnClickListener {
+            startRecording()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE)
+            permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startRecording() {
+        if (!permissionGranted) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE)
+            return
         }
     }
 }
