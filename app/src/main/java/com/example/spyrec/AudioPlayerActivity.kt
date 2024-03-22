979b0f4315a3d670387eae7c6e9fa2f1eaf5b995
tonysodeep <hynguyen2727@gmail.com>
2024-03-22 15:46:44 +0700

    completed AudioPlayerActivity

--- app/src/main/java/com/example/spyrec/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/spyrec/AudioPlayerActivity.kt
@@ -9,12 +9,15 @@ import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import com.example.spyrec.databinding.ActivityAudioPlayerBinding
+import java.text.DecimalFormat
+import java.text.NumberFormat
 
 class AudioPlayerActivity : AppCompatActivity() {
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var binding: ActivityAudioPlayerBinding
     private lateinit var runnable: Runnable
     private lateinit var handler: Handler
+
     private var delay = 1000L
     private var jumpValue = 1000
     private var playbackSpeed = 1.0f
@@ -28,15 +31,27 @@ class AudioPlayerActivity : AppCompatActivity() {
         var filePath = intent.getStringExtra("filepath")
         var fileName = intent.getStringExtra("filename")
 
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        binding.toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+        binding.tvFilename.text = fileName
+
         mediaPlayer = MediaPlayer()
         mediaPlayer.apply {
             setDataSource(filePath)
             prepare()
         }
 
+        binding.tvTrackDuration.text = dateFormat(mediaPlayer.duration)
+
         handler = Handler(Looper.getMainLooper())
         runnable = Runnable {
             binding.seekBar.progress = mediaPlayer.currentPosition
+            binding.tvTrackProgress.text = dateFormat(mediaPlayer.currentPosition)
             handler.postDelayed(runnable, delay)
         }
 
@@ -99,5 +114,26 @@ class AudioPlayerActivity : AppCompatActivity() {
                 ResourcesCompat.getDrawable(resources, R.drawable.ic_play_circle, theme)
             handler.removeCallbacks(runnable)
         }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        mediaPlayer.stop()
+        mediaPlayer.release()
+        handler.removeCallbacks(runnable)
+    }
+
+    private fun dateFormat(duration: Int): String {
+        var d = duration / 1000
+        var s = d % 60
+        var m = (d / 60 % 60)
+        var h = ((d - m * 60) / 360).toInt()
+        val f: NumberFormat = DecimalFormat("00")
+        var str = "$m:${f.format(s)}"
+
+        if (h > 0) {
+            str = "$h:$str"
+        }
+        return str
     }
 }
